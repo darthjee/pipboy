52ca6119c0ff1f14fe6772c42bfdf34543519d68
darthjee <darthjee@gmail.com>
2025-05-06 14:44:43 -0300

    Initialize

--- pipboy/LedInterface.h
+++ pipboy/LedInterface.h
@@ -1,14 +1,18 @@
 #ifndef LED_INTERFACE_H
 #define LED_INTERFACE_H
 
-#define LED_PIN_R 16
-#define LED_PIN_G 255
-#define LED_PIN_B 2
+#include <Arduino.h> // Necessário para o tipo uint8_t
 
 class LedInterface {
 public:
-    // Construtor vazio
-    LedInterface() {}
+    // Construtor que recebe os pinos RGB
+    LedInterface(uint8_t pin_r, uint8_t pin_g, uint8_t pin_b)
+        : pin_r(pin_r), pin_g(pin_g), pin_b(pin_b) {}
+
+private:
+    uint8_t pin_r; // Pino para o LED vermelho
+    uint8_t pin_g; // Pino para o LED verde
+    uint8_t pin_b; // Pino para o LED azul
 };
 
 #endif // LED_INTERFACE_H
--- pipboy/StatusLed.h
+++ pipboy/StatusLed.h
@@ -2,10 +2,13 @@
 #define LED_H
 
 #include "LedInterface.h" 
+#define LED_PIN_R 16
+#define LED_PIN_G 255
+#define LED_PIN_B 2
 
 class StatusLed {
 public:
-   StatusLed() {}
+    StatusLed() : interface(LED_PIN_R, LED_PIN_G, LED_PIN_B) {}
 
     LedInterface interface;
 };
